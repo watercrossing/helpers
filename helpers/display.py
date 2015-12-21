782e1de9e1de6a94985bd1549b6029e6c9f202ab
watercrossing <ingolf.becker@googlemail.com>
2015-12-21 16:37:40 +0000

    Add colourmaps table functions

--- helpers/display.py
+++ helpers/display.py
@@ -9,6 +9,7 @@ class Table(object):
     regexps = [(re.compile(r'(?<![\\])([#%&_\\{\\}])'),r'\\\1'), (re.compile(r'(?<![\\])([\^~])'),r'\\\1{}'), 
                (re.compile(r'(?<![\\])\\(?=\\)'),r'\\textbackslash{}')]
     
+    TEST = 125
     @classmethod
     def fixStringForLaTeX(cls, string):
         for r, t in cls.regexps:
@@ -27,15 +28,61 @@ class Table(object):
         self.title = ""
         self.reference = ''
         self.header = None
-    
-    def setHeader(self, row = []):
+        self.latexComment = ""
+        self.headerRotate = False
+        self.headerColours = []
+        self.cellColours = []
+
+    @staticmethod
+    def _addColoursToLatexRow(row, colours):
+        return row if not colours else [t if not c else "\\cellcolor[RGB]{%3d,%3d,%3d} %s" %(c[0],c[1],c[2],t) 
+                                       for c, t in zip(colours, row)]
+
+    def setHeader(self, row = [], cellColours = [], rotate=False):
         assert len(self.columns) == len(row)
+        if cellColours:
+            assert len(self.columns) == len(cellColours)
+            assert all(len(x) == 4 for x in cellColours if x)
+            # We can't handle alphas
+            assert all(x[3] == 255 for x in cellColours if x)
+        if rotate:
+            if type(rotate) == type(list()):
+                assert len(rotate) == len(self.columns)
+            self.headerRotate = rotate
+        self.headerColours = cellColours
         self.header = row
     
-    def addRow(self, row = []):
+    def addRow(self, row = [], cellColours = []):
+        """
+        cellColours is a list of (r, g, b, 1) (one is the alpha), r,g,b integers [0,255]
+        """
         assert len(self.columns) == len(row)
+        if cellColours:
+            assert len(self.columns) == len(cellColours)
+            assert all(len(x) == 4 for x in cellColours if x)
+            # We can't handle alphas
+            assert all(x[3] == 255 for x in cellColours if x)
+        self.cellColours.append(cellColours)
         self.rows.append(row)
     
+    def fromNPArray(self, values, formatstr,  rowdesc=[], cellColours=None):
+        """
+        fills all the data from an np.ndarray. Sets call the cell background colour from another ndarray of 
+        bytes=True plt.cm, i.e. plt.cm.summer(values,bytes=True).
+        """
+        assert values.shape[1] + int(bool(rowdesc)) == len(self.columns)
+        if rowdesc:
+            assert len(rowdesc) == values.shape[0]
+        if cellColours is not None:
+            assert values.shape + (4,) == cellColours.shape
+            if rowdesc:
+                self.cellColours = [[False] + row.tolist() for row in cellColours]
+            else: 
+                self.cellColours = cellColours.tolist()
+        else:
+           self.cellColours = [[] for _ in range(values.shape[0])]
+        self.rows = [rowdesc[i:i+1] + [formatstr %x for x in row] for i, row in enumerate(values)]
+        
     def setCaption(self, text = ""):
         self.caption = text
 
@@ -58,6 +105,18 @@ class Table(object):
         border: 1px double #000000 !important;
         border-style:none none double !important;
     }}
+    #{0} th.rotate {{
+        height: 140px;
+        white-space: nowrap;
+    }}
+    #{0} th.rotate > div {{
+        transform: 
+          /* Magic Numbers */
+          translate(25px, 51px)
+          /* 45 is really 360 - 45 */
+          rotate(315deg);
+        width: 30px;
+    }}
     #{0} td {{
         padding:0.2em 0.6em;
         border-style:none !important;
@@ -75,16 +134,52 @@ class Table(object):
         s += u'<table id="t%d">\n' %id(self)
         if self.caption:
             s += u'<caption>%s</caption>\n' %self.caption
+        if self.latexComment:
+            s += self.latexComment + u"\n</br>\n"
         if self.header: 
-            s += u'   <tr>' + u''.join([u'<th class="%s">%s</th>' %(self.columns[i],x) 
+            s += u'   <tr>' + u''.join([u'<th class="%s%s%s%s</th>' %(self.columns[i]," rotate\"><div>" if self.headerRotate and ((type(self.headerRotate) == type(list()) and self.headerRotate[i]) or (type(self.headerRotate) != type(list()) and i>0)) else "\">", x,"</div>" if self.headerRotate and ((type(self.headerRotate) == type(list()) and self.headerRotate[i]) or (type(self.headerRotate) != type(list()) and i>0)) else "" ) 
                                         for i, x in enumerate(self.header)]) + u'</tr>\n'
-        for row in self.rows:
-            s += u'   <tr>' + u''.join([u'<td class="%s">%s</td>' %(self.columns[i],x) 
+        for row,rowcol in zip(self.rows,self.cellColours):
+            s += u'   <tr>' + u''.join([u'<td class="%s"%s>%s</td>' %(self.columns[i],u"" if not rowcol or not rowcol[i] else u" bgcolor=\"#%2X%2X%2X\"" %(rowcol[i][0],rowcol[i][1],rowcol[i][2]),x) 
                                         for i, x in enumerate(row)]) + u'</tr>\n'
         
             
         s += u'</table>'
         return s
+
+    def compileLatexTable(self):
+        """
+        Returns touple (header, content), where header is a list and content is a list of lists.
+        """
+        if self.header:
+            hc = [self.fixStringForLaTeX(x) for x in self.header]
+            if self.headerRotate:
+                if type(self.headerRotate) == type(list()):
+                    rots = self.headerRotate
+                else:
+                    rots = [False] + [True]*(len(self.header) -1)
+                rots = ["" if not x else "\\rot%s{" %("" if type(x) == type(True) else "[%s]" %s) for x in rots]
+            hc = ["%s%s%s" %(r, c, "}" if r else "") for r,c in zip(rots,hc)]
+        
+
+            header = Table._addColoursToLatexRow(hc, self.headerColours)
+        else:
+            header = None
+        rows = [Table._addColoursToLatexRow([self.fixStringForLaTeX(x) for x in row],cs) for row,cs in zip(self.rows, self.cellColours)]
+
+        #now fix spacings
+        rs = izip(*rows) if not header else izip(header,*rows)
+        lens = [max(len(str(x)) for x in y) for y in rs]
+        lens = [-x if self.columns[i] == 'l' else x for i,x in enumerate(lens)]
+        if header:
+            header = [self.fixElement(*x) for x in zip(header, [len(hx) for hx in hc], lens)]
+        rows = [[self.fixElement(x[0],len(self.fixStringForLaTeX(x[1])),x[2]) for x in zip(row, rawRow, lens)] for row, rawRow in zip(rows, self.rows)]
+        return header, rows
+
+    def fixElement(self, t, rawLen, width):
+        l = len(t)
+        lr = rawLen
+        return "%s%*s" %(t[0:l-lr], width/abs(width)*(abs(width)-(l-lr)), t[l-lr:])
         
     def _repr_latex_(self):
         if self.isFloat and not self.isLongTable:
@@ -97,15 +192,17 @@ class Table(object):
                 s += u"\n\n".join([u'%s %s' %(u'%%' if not self.isLongTable else "",self.fixStringForLaTeX(x)) 
                                    for x in self.caption.splitlines()]) + u'\n'
             s += "\\begin{center}\n"
+        if self.latexComment:
+            s += u''.join([u'%%%s\n' %x for x in self.latexComment.splitlines()])
         s += u"\\begin{%s}{%s}\n" %(u'tabular' if not self.isLongTable else u'longtable',
                                     u''.join(self.latexColumns or self.columns))
-        sp = self._getSpacingForRows()
+
+        header, rows = self.compileLatexTable() 
         if self.header:
             #s += " Code& F. in A& F. in B& F. in C& F. in D& Label\\\\ \\hline\\hline\\\\[-1.em]\n"
-            s += u'   ' + u" & ".join(['%*s' %(sp[i],self.fixStringForLaTeX(x))
-                        for i, x in enumerate(self.header)]) + u" \\\\ \\hline\\hline\\\\[-1.em]\n"
-        for row in self.rows:
-            s += u'   ' + u" & ".join(['%*s' %(sp[i],self.fixStringForLaTeX(x)) for i, x in enumerate(row)]) + u" \\\\[0.3em]\n"
+            s += u'   %s \\\\ \\hline\\hline\\\\[-1.em]\n' %(u" & ".join(header))
+        for row in rows:
+            s += u'   %s \\\\[0.3em]\n' %(u" & ".join(row))
         s += "\\end{%s}\n" %(u'tabular' if not self.isLongTable else u'longtable')
         if self.isFloat and not self.isLongTable:
             if self.caption:
@@ -117,12 +214,6 @@ class Table(object):
             s+= u"\\end{center}\n"
         return s
     
-    def _getSpacingForRows(self):
-        rs = izip(*self.rows) if not self.header else izip(self.header,*self.rows)
-        lens = [max(len(str(x)) for x in y) for y in rs]
-        lens = [-x if self.columns[i] == 'l' else x for i,x in enumerate(lens)]
-        return lens
-        
     @property
     def latex(self):
         return Math(self._repr_latex_())
@@ -282,7 +373,7 @@ def rstyle(ax):
 
     plt.rc('font', **{'family':'serif', 'serif':['Computer Modern Roman'], 
                                         'monospace': ['Computer Modern Typewriter'],
-                                        'sans-serif': ['Computer Modern Sans serif'})
+                                        'sans-serif': ['Computer Modern Sans serif']})
    
     if ax.legend_ <> None:
         lg = ax.legend_
