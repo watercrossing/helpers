b60e17c15386ac1fc5c629df44a710c0771c4e12
Ingolf Becker <ingolf.becker@googlemail.com>
2016-01-06 12:23:32 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,20 @@
 from setuptools import setup, find_packages
 
 setup(name='helpers',
-      version='0.01',
+      version='0.2',
       description='My little helpers',
+      url='https://github.com/watercrossing/helpers',
+      license='MIT',
       author='Ingolf Becker',
       author_email='ingolf.becker@googlemail.com',
       packages=find_packages(),
-      install_requires = ['IPython', 'lxml', 'dill', 'scipy', 'matplotlib', ]
+      install_requires = ['IPython', 'lxml', 'dill', 'scipy', 'matplotlib', ],
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 2',
+            'Topic :: Software Development :: Libraries',
+            'Topic :: Communications :: Typesetting :: LaTeX'
+            ]
      )
 
